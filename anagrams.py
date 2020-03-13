130d26718aa203a11cd4280cf1f019085e572320
Piero Madar <madarp@pobox.com>
2020-03-13 15:24:41 -0400

    relax test benchmarks.  Add correctness test.

--- anagrams.py
+++ anagrams.py
@@ -1,43 +1,31 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-""" anagrams
-    Command line interface that accepts a word file and returns a dictionary of
-    anagrams for that file.
-
-    Module provides a function find_anagrams which can be used to do the same
-    for an arbitrary list of strings.
-
 """
-__author__ = "???"
+Command line utility that accepts a word file and prints a dictionary of
+anagrams for that file.
+
+Module provides a function find_anagrams which can be used to do the same
+for an arbitrary list of strings.
+"""
 
 import sys
 
+# Your name here, and any other people/sources who helped.
+# Give credit where credit is due.
+__author__ = "???"
+
 
 def alphabetize(string):
-    """ alphabetize
-        Given a string, return a string that includes the same letters in
-        alphabetical order.
-
-        Example:
-
-        >>> print alphabetize('cab')
-        abc
-
-    """
+    """Returns alphabetized version of the string"""
     return "".join(sorted(string.lower()))
 
 
 def find_anagrams(words):
-    """ find_anagrams
-
-        Return a dictionary with keys that are alphabetized words and values
-        that are all words that, when alphabetized, match the key.
-
-        Example:
-
-        >>> print find_anagrams(['cat', 'dog', 'act'])
-        {'dgo': ['dog'], 'act': ['cat', 'act']}
-
+    """
+    Returns a dictionary with keys that are alphabetized words and values
+    that are all words that, when alphabetized, match the key.
+    Example:
+    {'dgo': ['dog'], 'act': ['cat', 'act']}
     """
     anagrams = {
         alphabetize(word): [
@@ -47,12 +35,18 @@ def find_anagrams(words):
     return anagrams
 
 
-if __name__ == "__main__":
-    # run find anagrams of first argument
-    if len(sys.argv) < 2:
-        print "Please specify a word file!"
+def main(args):
+    # run find_anagrams() on first argument filename
+    if len(args) < 1:
+        print("Please specify a word file!")
         sys.exit(1)
-    else:
-        with open(sys.argv[1], 'r') as handle:
-            words = handle.read().split()
-            print find_anagrams(words)
+
+    with open(args[0]) as f:
+        words = f.read().split()
+    anagram_dict = find_anagrams(words)
+    for k, v in anagram_dict.items():
+        print("{} : {}".format(k, v))
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
--- tests/test_anagrams.py
+++ tests/test_anagrams.py
@@ -1,37 +1,57 @@
 """ Test Suite for anagrams module. """
 import unittest
-from timeit import Timer
-from anagrams import find_anagrams
+import importlib
+import timeit
+import json
+import functools
 
 
 class TestAnagrams(unittest.TestCase):
     """
-        Benchmarking test case. We test actual functionality of find_anagrams
-        with doctests, which is why this test case excludes those unit tests.
+    Benchmarking test case. We test actual functionality of `find_anagrams`
+    with doctests, which is why this test case excludes those unit tests.
     """
+    def setUp(self):
+        module_name = 'anagrams'
+        """import the module(s) under test, in the context of this test fixture"""
+        try:
+            self.ana = importlib.import_module(module_name)
+        except ImportError:
+            self.fail('Unable to import module: ' + module_name)
 
     def run_find_anagrams(self, word_list, benchmark):
-        my_time = Timer(lambda: find_anagrams(word_list)).timeit(number=1)
-        my_time = round(my_time, 3)
+        """Helper func to time the find_anagrams() func"""
+        f = functools.partial(self.ana.find_anagrams, word_list)
+        t = timeit.Timer(f)
+        actual_time = round(t.timeit(number=1), 3)
         failure_text = (
             'find_anagrams took {} seconds, which exceeds the '
-            'benchmark of {} seconds'.format(my_time, benchmark)
+            'benchmark of {} seconds'.format(actual_time, benchmark)
             )
-        self.assertTrue(my_time <= benchmark, failure_text)
+        self.assertLessEqual(actual_time, benchmark, failure_text)
 
-    def test_short(self):
-        """ Test find_anagrams with short word list. """
+    def test_correct_result(self):
+        """Check the anagram dict result for correctness"""
         with open("words/short.txt") as f:
             short_list = f.read().split()
-        self.run_find_anagrams(short_list, 0.005)
+        actual_dict = self.ana.find_anagrams(short_list)
+        self.assertIsInstance(actual_dict, dict)
+        with open('tests/short_list.json') as f:
+            expected_dict = json.loads(f.read())
+        self.assertDictEqual(actual_dict, expected_dict)
 
+    def test_short(self):
+        """Check find_anagrams() func timing with short word list."""
+        with open("words/short.txt") as f:
+            short_list = f.read().split()
+        self.run_find_anagrams(short_list, 0.02)
 
     @unittest.skip("Remove this line once short test passes")
     def test_long(self):
-        """ Test find_anagrams with long word list. """
+        """Check find_anagrams() with long word list."""
         with open("words/long.txt") as f:
             long_list = f.read().split()
-        self.run_find_anagrams(long_list, 0.10)
+        self.run_find_anagrams(long_list, 0.30)
 
 
 if __name__ == '__main__':
